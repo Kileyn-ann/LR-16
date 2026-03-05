f58ba5fc80e52484ca9cd06b157e83ab1154e89e
Лиза <lizadan2008.s4@gmail.com>
2026-03-05 18:51:25 +0300

    Delete pet_shop/shop directory

--- pet_shop/shop/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('about/', views.about_shop, name='toys_shop'),
-    path('author/', views.about_author, name='Lisa'),
-]
